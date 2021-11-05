72403e4c5a56c91b38aec82566d32992d3310ca4
tqp <timothyqpowers@gmail.com>
2021-11-04 22:06:32 -0400

    Refactoring and formatting.

--- gps-single.py
+++ gps-single.py
@@ -1,37 +1,37 @@
 #!/usr/bin/env python3
 
-import datetime
-import serial
 import time
 
-from datetime import timezone
+import serial
 
 port = "/dev/ttyACM0"
 
-def parse_gps(fix, data):
+
+def parse_gps(data):
     data = data.decode('UTF-8')
     print("raw:", data)
+    fix = False
 
     # Check Fix
-    if ((len(data) > 0) & (data[0:6] == '$GPGGA')):
+    if (len(data) > 0) & (data[0:6] == '$GPGGA'):
         split_data = data.split(",")
         status = split_data[6]
-        if(status.isdigit()):
+        if status.isdigit():
             fix = int(status) > 0
             print("Fix: %s %s" % (fix, str(split_data)))
-            time=split_data[2]
-            lat=split_data[3]
-            lat_dir=split_data[4]
-            lng=split_data[5]
-            lng_dir=split_data[6]
-            quality=split_data[7]
-            num_satellite=data[8]
-            altitude=split_data[10]
-            altitude_unit=split_data[11]
-    
-    if (fix):
+            utc = split_data[2]
+            lat = split_data[3]
+            lat_dir = split_data[4]
+            lng = split_data[5]
+            lng_dir = split_data[6]
+            quality = split_data[7]
+            num_satellite = data[8]
+            altitude = split_data[10]
+            altitude_unit = split_data[11]
+
+    if fix:
         print("Continuing...")
-        #if ((len(data) > 0) & (data[0:6] == '$GPVTG')):
+        # if ((len(data) > 0) & (data[0:6] == '$GPVTG')):
         #    print("Receiving GPVTG Data...")
         #    split_data = data.split(",")
         #    print("Type: %s" % (split_data[1]))
@@ -42,19 +42,15 @@ def parse_gps(fix, data):
 
 print("Receiving GPS data")
 
-fix = False
-lat = 0
-lng = 0
-
 gpsConnection = False
-while gpsConnection != True:
+while not gpsConnection:
     try:
         ser = serial.Serial(port, baudrate=9600, timeout=0.5)
         gpsConnection = True
-    except:
-        print("Could not access GPS. Trying again in 5 seconds...")
+    except Exception as e:
+        print("Could not access GPS. Trying again in 5 seconds..." + str(e))
         time.sleep(5)
 
 while True:
     raw_data = ser.readline()
-    parse_gps(fix, raw_data)
+    parse_gps(raw_data)
